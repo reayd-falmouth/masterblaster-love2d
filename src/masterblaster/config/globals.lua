680f013208ea1c00aa470195b1ad1b3e565da763
reayd-falmouth <dr323090@falmouth.ac.uk>
2025-03-17 12:45:36 +0000

    Screen resolution can be changed

--- src/masterblaster/config/globals.lua
+++ src/masterblaster/config/globals.lua
@@ -3,9 +3,9 @@ log = require("utils.logging")
 log.LOG_LEVEL = log.INFO
 
 VERSION = "v1.0"
-VIRTUAL_WIDTH = 640
-VIRTUAL_HEIGHT = 512
+VIRTUAL_WIDTH = 320
+VIRTUAL_HEIGHT = 256
 ENABLE_MUSIC = false
-DEBUG = true
+DEBUG = false
 TILE_SIZE = 16
 LOG_LEVEL = ERROR
